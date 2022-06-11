6eee6307676acf42882bffc6c6b6b2d2b3100b01
Peter Keating <pete.keating@etchuk.com>
2022-06-11 08:57:32 +0100

    v0.1.1

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Orchard Core module for controlling cache-control response headers on content items.",
     Name = "Cache Control",
-    Version = "0.1.0",
+    Version = "0.1.1",
     Website = "https://etchuk.com"
 )]
